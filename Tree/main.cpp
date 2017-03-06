d7698f20e6c1f132134b95c3e9e6af5329110fb3
aml295 <aml295@georgetown.edu>
2017-03-05 20:47:13 -0600

    Revert "New update"
    This reverts commit f0bbcf10593cc4cfea020c96ec3875839cc4de1d.

---
+++ Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tree
+//
+//  Created by Ahmed Latif on 2/11/17.
+//  Copyright © 2017 Ahmed Latif. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
